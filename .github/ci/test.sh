a02fb51a68fc4b30efe1e778ecf41b66cfe02fe0
Jacob Rosenthal <jacobrosenthal@gmail.com>
2024-03-02 19:51:25 -0700

    first

---
+++ .github/ci/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## on push branch~=gh-readonly-queue/main/.*
+## on pull_request
+
+set -euo pipefail
+
+export RUSTUP_HOME=/ci/cache/rustup
+export CARGO_HOME=/ci/cache/cargo
+export CARGO_TARGET_DIR=/ci/cache/target
+
+cargo test --manifest-path ./something/Cargo.toml 
