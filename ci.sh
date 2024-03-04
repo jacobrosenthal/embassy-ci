0cf580fbcbd239e62989eb55fe72df0a1f160def
Jacob Rosenthal <jacobrosenthal@gmail.com>
2024-03-04 15:54:24 -0700

    use cargo batch

--- ci.sh
+++ ci.sh
@@ -8,7 +8,8 @@ if [[ -z "${CARGO_TARGET_DIR}" ]]; then
     export CARGO_TARGET_DIR=target_ci
 fi
 
-cargo +nightly build -Zunstable-options --release --manifest-path tests/nrf52840/Cargo.toml --target thumbv7em-none-eabi --out-dir out/tests/nrf52840-dk 
+cargo batch  \
+    --- build --release --manifest-path tests/nrf52840/Cargo.toml --target thumbv7em-none-eabi --out-dir out/tests/nrf52840-dk \
 
 if [[ -z "${TELEPROBE_TOKEN-}" ]]; then
     echo No teleprobe token found, skipping running HIL tests
